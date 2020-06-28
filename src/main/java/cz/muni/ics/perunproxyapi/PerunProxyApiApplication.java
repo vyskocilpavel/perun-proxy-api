8fc79e1f1e9add7994461f0d1ad735a0c52d2a11
Pavel Vyskočil <vyskocilpavel@muni.cz>
2020-06-28 15:47:39 +0200

    Initial commit

---
+++ src/main/java/cz/muni/ics/perunproxyapi/PerunProxyApiApplication.java
@@ -0,0 +1,13 @@
+package cz.muni.ics.perunproxyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerunProxyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerunProxyApiApplication.class, args);
+    }
+
+}
